85754de63654ec9da3b68bcd355fbb29e0ffcf28
Roman Mosin <mosinra90@yandex.ru>
2020-07-04 11:13:01 +0500

    hw6

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,2 +1,39 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class Test2 {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1, 1, 1, 4, 4, 1, 4, 4}, true},
+                {new int[]{1, 1, 1, 1, 1, 1, 1, 1}, false},
+                {new int[]{4, 4, 4, 4}, false},
+                {new int[]{1, 4, 4, 1, 1, 4, 3}, false}
+        });
+    }
+    private int[] in;
+    private boolean out;
+
+    public Test2 (int[] in, boolean out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    private Task2 task2;
+
+    @Before
+    public void startTest() {
+        task2 = new Task2();
+    }
+
+    @Test
+    public void structure() {
+        Assert.assertEquals(Task2.structure(in), out);
+    }
 }
